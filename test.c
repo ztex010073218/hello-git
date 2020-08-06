9941d37d540e7513f381d434cacd87ae0b4cf39e
ztex <ztex010073218@gmail.com>
2020-08-06 21:37:45 +0800

    add gcd function

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 //it is a tset c file for git 
 #include <stdio.h>
 
+int gcd(int a,int b)
+{
+	if(b==0)
+	{
+		return a;
+	}
+	else
+	{
+		return(b,a%b);
+	}
+}
+
 int main(void)
 {
-	printf("hello\n");
+	int num1,num2;
+	printf("hello it is gcd function, input two numbers,please\n");
+	scanf("%d %d",&num1,&num2);
+	printf("%d\n",gcd(num1,num2));
 	return 0;
 }
